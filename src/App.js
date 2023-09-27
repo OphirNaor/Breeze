17838b1fd9e15b3315ea78e31e7189a4e9195b6f
OphirNaor <98401468+OphirNaor@users.noreply.github.com>
2023-09-27 10:03:53 +0300

    create pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { Box } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
+import Menu from "./pages/Menu";
+import Branches from "./pages/Branches";
+import About from "./pages/About";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/branches' element={<Branches />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<ContactUs />} />
       </Routes>
       {/* <Footer /> */}
     </Box>
