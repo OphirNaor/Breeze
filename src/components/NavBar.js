edc94ce0a185fe0474478b7508ef099bfc993b0d
OphirNaor <98401468+OphirNaor@users.noreply.github.com>
2023-09-27 08:45:30 +0300

    create first folders

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <Box>
+            <Link to='/'>Home</Link>
+        </Box>
+    )
+}
+
+export default NavBar
