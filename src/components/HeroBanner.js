914467335377c1128c3c158a2b45ee430d4e4e81
OphirNaor <98401468+OphirNaor@users.noreply.github.com>
2023-09-27 13:55:54 +0300

    Update HeroBanner.js

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,6 +1,7 @@
 import { Stack, Typography } from "@mui/material";
 import { makeStyles } from 'tss-react/mui';
 import HeroBannerImage from '../assets/images/HeroBannerImage.png';
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles()(theme => ({
   hero: {
@@ -10,10 +11,10 @@ const useStyles = makeStyles()(theme => ({
     position: 'relative',
     width: '100%',
     padding: '3rem 0',
-    backgroundPosition: '50% 60%',
+    backgroundPosition: '50% 65%',
     height: 'clamp(20rem,55vw,45rem)',
     textAlign: 'center',
-    opacity: '0.7'
+    // opacity: '0.7'
   }
 }));
 
@@ -22,16 +23,19 @@ const HeroBanner = () => {
 
   return (
     <Stack justifyContent='center' alignItems='center' className={classes.hero}>
-      <Typography fontWeight={700} sx={{ fontSize: { lg: '55px', xs: '40px' } }} mb='23px' mt='30px' color='#e74848'>
-        {/* Unleash Your Inner Athlete! */}
-      </Typography>
-      <Typography fontSize='22px' lineHeight='35px' mb={4} color='#e74848'>
-        {/* check out the most effective exercises */}
-      </Typography>
-      <Stack>
-        {/* <a href="#exercises" style={{
-            marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#e74848', padding: '14px', fontSize: '22px', textTransform: 'none', color: '#000000', borderRadius: '4px'
-        }}>Explore Exercises</a> */}
+      <Stack alignItems='center' position='absolute' bottom='18%'>
+        <Typography fontWeight={700} sx={{ fontSize: { lg: '45px', xs: '40px' } }} mt='40px' color='#00000'>
+          Blending Health and Flavor <br />
+          One Sip at a Time
+        </Typography>
+        <Typography fontSize='22px' lineHeight='35px' mb={4} color='#00000'>
+          check out the best smoothies
+        </Typography>
+        <Stack>
+          <Link to='/menu' style={{
+            textDecoration: 'none', width: '200px', textAlign: 'center', background: '#e74848', padding: '14px', fontSize: '22px', textTransform: 'none', color: '#000000', borderRadius: '4px'
+          }}>I Want It!</Link>
+        </Stack>
       </Stack>
     </Stack >
   )
